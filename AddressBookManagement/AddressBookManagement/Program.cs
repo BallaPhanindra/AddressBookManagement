74f9599d853bcd52cd849f7328bebda6fba1f9d1
Balla <ballaphanindra@outlook.com>
2022-07-14 22:44:04 +0530

    Committing CreationOfContacts

--- AddressBookManagement/AddressBookManagement/Program.cs
+++ AddressBookManagement/AddressBookManagement/Program.cs
@@ -2,11 +2,17 @@
 
 namespace AddressBookManagement
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Welcome to Address Book\n");
+
+            CreateContact.CreateAContact();
+            //AddNewContact.AddNewContacts();
+            //UpdateExistingContact.UpdateContact();
+            //DeleteContact.ContactDelete();
+            Console.ReadKey();
         }
     }
 }
